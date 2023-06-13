ba09b8e0b8ff69e6bf52a33a8c3cb198c9c4652d
RafaVera <jr.vera.ma@gmail.com>
2023-06-13 22:17:52 +0200

    Initial commit

---
+++ src/test/java/dev/rafael/springbootneo4j/SpringBootNeo4jApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.rafael.springbootneo4j;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootNeo4jApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
