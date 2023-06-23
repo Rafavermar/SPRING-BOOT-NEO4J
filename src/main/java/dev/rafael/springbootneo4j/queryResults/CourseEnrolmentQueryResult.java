d64401a3ee5de397a84641a17d3f15cede1743ea
RafaVera <jr.vera.ma@gmail.com>
2023-06-23 11:15:13 +0200

    starting Endpoint create realtionships

---
+++ src/main/java/dev/rafael/springbootneo4j/queryResults/CourseEnrolmentQueryResult.java
@@ -0,0 +1,28 @@
+package dev.rafael.springbootneo4j.queryResults;
+
+import dev.rafael.springbootneo4j.models.Course;
+import dev.rafael.springbootneo4j.models.User;
+
+public class CourseEnrolmentQueryResult {
+    private User user;
+    private Course course;
+
+    public CourseEnrolmentQueryResult() {
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser( User user ) {
+        this.user = user;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse( Course course ) {
+        this.course = course;
+    }
+}
--- src/main/java/dev/rafael/springbootneo4j/repositories/UserRepository.java
+++ src/main/java/dev/rafael/springbootneo4j/repositories/UserRepository.java
@@ -1,7 +1,9 @@
 package dev.rafael.springbootneo4j.repositories;
 
 import dev.rafael.springbootneo4j.models.User;
+import dev.rafael.springbootneo4j.queryResults.CourseEnrolmentQueryResult;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
+import org.springframework.data.neo4j.repository.query.Query;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 import java.util.Optional;
@@ -11,4 +13,12 @@ public interface UserRepository extends Neo4jRepository<User, Long> {
     Optional<User> findUserByUsername(String username);
 
     boolean existsByUsername( String username );
+@Query("Match (user:User), (course:Course) WHERE user.username = $username AND course.identifier = $identifier" +
+        "RETURN EXISTS((user)-[:ENROLLED_IN]->(course))")
+    boolean findEnrolmentStatus(String username, String identifier);
+
+@Query("Match (user:User), (course:Course) WHERE user.username = $username AND course.identifier = $identifier" +
+        "CREATE (user)--[:ENROLLED_IN]->(course) RETURN user, course")
+CourseEnrolmentQueryResult createEnrolmentRelationship(String username, String identifier);
+
 }
